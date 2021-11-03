0606208cd8adbd10bc805758a8d95a9a65425947
mgorgeov97 <matthewggvey87@hotmail.com>
2021-11-03 09:44:02 -0230

    trying to draw the fruit on canvas, color problem

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+const apple = {
+    x: 200,
+    y: 200,
+    size: 30,
+    color: 'red',
+    dx: 5,
+    dy: 4
+}
+
+function drawApple() {
+    ctx.fillRect(apple.x, apple.y, apple.size, apple.size);
+    ctx.fillStyle = apple.color;
+}
+
+drawApple();
